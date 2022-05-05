192c8abb14b0bb6bd59182870b4e3aa25819c673
GuillermoGildeEusebio <guillermogildeeusebio@gmail.com>
2022-05-05 20:52:00 +0200

    Creacion de pruebas para la explicacion

--- TFG/src/test/java/com/TFG/springbootApp/ReglaExplicacionTest.java
+++ TFG/src/test/java/com/TFG/springbootApp/ReglaExplicacionTest.java
@@ -69,7 +69,7 @@ class ReglaExplicacionTest {
 		
 		Collection<Integer> posicionExplicaciones = explicacionDetectadas.values();
 		
-		assertTrue(posicionExplicaciones.contains(2));
+		assertTrue(posicionExplicaciones.contains(1));
 		
 	}
 	
@@ -98,7 +98,7 @@ class ReglaExplicacionTest {
 	
 	@Test
 	void Prueba5() throws Exception {
-		String text = "Pablo, mi sobrino, salió a caminar por la plaza.";
+		String text = "Pablo, que es mi sobrino, salió a caminar por la plaza.";
 		
 		Map<String[] ,Integer> explicacionDetectadas = new HashMap<String[] , Integer>();
 		
@@ -117,7 +117,7 @@ class ReglaExplicacionTest {
 	
 	@Test
 	void Prueba6() throws Exception {
-		String text = "El payaso, el animador de la fiesta de cumpleaños, nos hizo reír mucho";
+		String text = "El payaso,que es el animador de la fiesta de cumpleaños, nos hizo reír mucho";
 		
 		Map<String[] ,Integer> explicacionDetectadas = new HashMap<String[] , Integer>();
 		
@@ -130,6 +130,7 @@ class ReglaExplicacionTest {
 		Collection<Integer> posicionExplicaciones = explicacionDetectadas.values();
 		
 		assertTrue(posicionExplicaciones.contains(1));
+		
 		
 	}
 	
@@ -144,14 +145,35 @@ class ReglaExplicacionTest {
 		explicacionDetectadas = explicacion.detectorExplicaciones(text);
 				
 				
-		assertTrue(explicacionDetectadas.size() == 0);
+		assertTrue(explicacionDetectadas.size() == 1);
+		
+		Collection<Integer> posicionExplicaciones = explicacionDetectadas.values();
+		
+		assertTrue(posicionExplicaciones.contains(1));
 		
 	}
 	
 	
 	@Test
 	void Prueba8() throws Exception {
-		String text = "Mis amigas, las mujeres más generosas, siempre me acompañan.";
+		String text = "Ramón, el portero del edificio, me reparó el calefón.";
+		
+		Map<String[] ,Integer> explicacionDetectadas = new HashMap<String[] , Integer>();
+		
+		
+		explicacionDetectadas = explicacion.detectorExplicaciones(text);
+				
+				
+		assertTrue(explicacionDetectadas.size() == 0);
+		
+
+		
+	}
+	
+	
+	@Test
+	void Prueba9() throws Exception {
+		String text = "Marta, que es experta en botánica, regará las plantas mientras esté de viaje.";
 		
 		Map<String[] ,Integer> explicacionDetectadas = new HashMap<String[] , Integer>();
 		
@@ -169,26 +191,9 @@ class ReglaExplicacionTest {
 	
 	
 	@Test
-	void Prueba9() throws Exception {
-		String text = "Mis amigas, las mujeres más generosas que existen, siempre me acompañan.";
-		
-		Map<String[] ,Integer> explicacionDetectadas = new HashMap<String[] , Integer>();
-		
-		
-		explicacionDetectadas = explicacion.detectorExplicaciones(text);
-				
-				
-		assertTrue(explicacionDetectadas.size() == 0);
-		
-	}
-	
-	
-	@Test
 	void Prueba10() throws Exception {
-		String text = "Los chinos, que tienen una visión más dual y menos maniquea de las cosas, suelen decir que un error es una oportunidad. En el mundo occidental, en cambio, un error es visto siempre como un fracaso; por eso me parece interesante pararse por un momento a pensar cuánto debemos a nuestras equivocaciones. Hace poco estuvo en Madrid el físico francés Gérard Mourou, al que lo que podía haber sido un trágico accidente laboral le valió un Premio Nobel. "
-				+ "En 1992, Detao Du, un joven estudiante chino, ayudante del profesor Mourou, estaba alineando los láseres en una máquina de laboratorio cuando la potente luz le hirió un ojo. Al llevarlo a urgencias, el médico preguntó al profesor que había acompañado a Detao al hospital qué clase de láser era aquel, porque nunca había visto una herida tan perfecta y focalizada. Veintitantos años más tarde, y gracias a este comentario que dejó cavilando a Mourou, la técnica de amplificación de pulso gorjeado es una herramienta común en oftalmología y se usa para corregir la miopía, la hipermetropía y el astigmatismo. "
-				+ "El Premio Nobel de Física de 2018 no es el único beneficiario de los errores afortunados. Se cuentan por docenas los descubrimientos debidos a una chapuza, una negligencia o una colosal metedura de pata. El ejemplo más conocido tal vez sea el descubrimiento de la penicilina. Fleming no buscaba beneficiar a la humanidad ni cambiar el curso de la historia cuando descuidó las muestras de laboratorio que tenía a su cargo y una cepa de estafilococos que estaba estudiando se llenó de moho.";
-		
+		String text = "Maria, a quien hecho mucho de menos, vendrá a visitarme este fin de semana por mi cumpleaños.";
+
 		Map<String[] ,Integer> explicacionDetectadas = new HashMap<String[] , Integer>();
 		
 		
@@ -200,7 +205,7 @@ class ReglaExplicacionTest {
 		Collection<Integer> posicionExplicaciones = explicacionDetectadas.values();
 		
 		
-		assertTrue(posicionExplicaciones.contains(8));
+		assertTrue(posicionExplicaciones.contains(1));
 		
 	}
 	
